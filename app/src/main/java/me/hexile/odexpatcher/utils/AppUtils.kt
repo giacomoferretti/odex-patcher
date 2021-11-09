d13ed9711f3a0dac69f82d439cad0928ad668907
Giacomo Ferretti <me@hexile.me>
2021-11-09 19:02:37 +0100

    chore: update copyright

--- app/src/main/java/me/hexile/odexpatcher/utils/AppUtils.kt
+++ app/src/main/java/me/hexile/odexpatcher/utils/AppUtils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Giacomo Ferretti
+ * Copyright 2020-2021 Giacomo Ferretti
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
