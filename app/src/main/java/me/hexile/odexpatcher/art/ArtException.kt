f530289a337fcd8dcf1d5f44d40486f12e949ad7
Giacomo Ferretti <me@hexile.me>
2021-11-09 18:52:21 +0100

    fix: rewrite OatFile and VdexFile
    Added missing files to commit

---
+++ app/src/main/java/me/hexile/odexpatcher/art/ArtException.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020-2021 Giacomo Ferretti
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.hexile.odexpatcher.art
+
+import me.hexile.odexpatcher.utils.toHexString
+
+class ArtException : Exception {
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+    constructor(
+        message: String,
+        expected: ByteArray,
+        got: ByteArray
+    ) : super("$message Expected: ${expected.toHexString()}, got ${got.toHexString()}")
+}
---
+++ app/src/main/java/me/hexile/odexpatcher/art/ArtPatcher.kt
@@ -0,0 +1,92 @@
+package me.hexile.odexpatcher.art
+
+import me.hexile.odexpatcher.utils.readBytes
+import me.hexile.odexpatcher.utils.toHexString
+import java.io.File
+import java.io.RandomAccessFile
+
+interface ArtInfo {
+    val version: String
+    val dexFileCount: Int
+    val dexChecksumOffset: Long
+    val headerSize: Long
+    //val headerFields: Int
+    fun parseChecksum(pos: Long, checksums: ArrayList<Pair<Long, ByteArray>>): Long
+    //fun getHeaderSize(): Long
+}
+
+abstract class ArtPatcher(private val file: File) {
+    protected abstract fun checkIfValid(raf: RandomAccessFile)
+    protected abstract fun parseHeader(raf: RandomAccessFile): ArtInfo
+
+    var header: ArtInfo
+
+    private val _checksums: ArrayList<Pair<Long, ByteArray>>
+    val checksums: List<Pair<Long, ByteArray>>
+        get() = _checksums.toList()
+
+    init {
+        RandomAccessFile(file, "r").use { raf ->
+            checkIfValid(raf)
+            header = parseHeader(raf)
+            _checksums = parseChecksums()
+        }
+    }
+
+    fun patch(checksums: ArrayList<ByteArray>) {
+        if (checksums.isEmpty()) {
+            throw IllegalArgumentException("Cannot be empty")
+        } else if (checksums.size != this._checksums.size) {
+            throw IllegalArgumentException("Must be same size ${checksums.size} != ${this._checksums.size}")
+        }
+
+        RandomAccessFile(file, "rw").use { raf ->
+            this._checksums.asSequence().withIndex().iterator().forEach {
+                println("PATCHING ${it.index}")
+
+                val source = raf.readBytes(it.value.first, 4)
+                val sourceCache = it.value.second
+                val new = checksums[it.index]
+
+                println(" -> ${source.toHexString()} = ${new.toHexString()} [${sourceCache.toHexString()}]")
+
+                raf.seek(it.value.first)
+                raf.write(checksums[it.index]) // java.lang.IndexOutOfBoundsException: Invalid index 0, size is 0
+            }
+        }
+    }
+
+    private fun parseChecksums(): ArrayList<Pair<Long, ByteArray>> {
+        val checksums = ArrayList<Pair<Long, ByteArray>>()
+
+        var cursor = header.dexChecksumOffset
+        for (i in 0 until header.dexFileCount) {
+            cursor = header.parseChecksum(cursor, checksums)
+        }
+
+        return checksums
+    }
+
+    protected fun toStringContents(): String {
+        val checksumsString = checksums.iterator().let {
+            if (!it.hasNext()) {
+                "{}"
+            } else {
+                var result = "{"
+                while (true) {
+                    val entry = it.next()
+                    result += "${entry.first}=${entry.second.toHexString()}"
+                    if (!it.hasNext()) break
+                    result += ", "
+                }
+                "$result}"
+            }
+        }
+
+        return "file=$file, header=$header, checksums=$checksumsString)"
+    }
+
+    override fun toString(): String {
+        return "ArtPatcher(${toStringContents()})"
+    }
+}
