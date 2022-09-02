7d677198ce31ca4fe4151103f894e3d76fe23c75
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-09-02 22:29:46 +0200

    fix: add internal keyword to private utilities

--- library/src/main/java/com/giacomoferretti/odexpatcher/library/internal/Utils.kt
+++ library/src/main/java/com/giacomoferretti/odexpatcher/library/internal/Utils.kt
@@ -1,18 +1,18 @@
-@file:JvmName("#") // Hide from JVM
+// @file:JvmName("#") // Hide from JVM
 
 package com.giacomoferretti.odexpatcher.library.internal
 
 import android.annotation.SuppressLint
 
 @JvmSynthetic
-fun getProperty(key: String): String {
+internal fun getProperty(key: String): String {
     return getProperty(key, "")
 }
 
 // https://android.googlesource.com/platform/frameworks/base/+/refs/tags/android-13.0.0_r1/core/java/android/os/SystemProperties.java#163
 @JvmSynthetic
 @SuppressLint("PrivateApi")
-fun getProperty(key: String, def: String): String {
+internal fun getProperty(key: String, def: String): String {
     val c = Class.forName("android.os.SystemProperties")
     val method = c.getMethod("get", String::class.java, String::class.java)
     return method.invoke(c, key, def) as String
